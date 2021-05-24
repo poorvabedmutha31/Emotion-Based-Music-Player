20a53b51bc324c8f52f89a69cb4111f1971b5042
Poorva Bedmutha <poorvabedmutha312000@gmail.com>
2021-05-24 20:53:54 +0530

    Adding Spotify API requirements

--- app/src/main/java/com/lampa/emotionrecognition/MainActivity.java
+++ app/src/main/java/com/lampa/emotionrecognition/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -50,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 0;
     private static final int TAKE_PHOTO_REQUEST_CODE = 1;
+    private static final String CLIENT_ID = "61b6ded3327240c48e97552d4779acc2";
+    private static final String SECRET_ID ="74e4a6bca21040f18f395e204da43a67";
+    private static final String REDIRECT_URI = "https://localhost.com/callback/";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     private final String MODEL_FILE_NAME = "simple_classifier.tflite";
 
@@ -69,6 +81,59 @@ public class MainActivity extends AppCompatActivity {
     private Uri mCurrentPhotoUri;
 
     private Map<String, List<Pair<String, String>>> mClassificationResult;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
