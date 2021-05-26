8f052f1baa76923ae7a523f3926442be0bcd9210
Prathamesh03 <iamprathamesh7cool@gmail.com>
2021-05-26 09:52:40 +0530

    Some changes done

--- app/src/main/java/com/lampa/emotionrecognition/SplashActivity.java
+++ app/src/main/java/com/lampa/emotionrecognition/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,8 +12,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         getSupportActionBar().hide();
+//        startActivity(new Intent(this,MainActivity.class));
+
+
         Thread thread=new Thread(){
             @Override
             public void run() {
@@ -25,8 +28,15 @@ public class SplashActivity extends AppCompatActivity {
                 finally {
                     Intent intent=new Intent(SplashActivity.this,MainActivity.class);
                     startActivity(intent);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    });
                 }
             }
         };thread.start();
+
     }
 }
